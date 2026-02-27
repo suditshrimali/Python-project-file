2fc52fb7208ba597cbf8f6bc5e6eab56a7a82239
Sudit Shrimali <suditshrimali@gmail.com>
2026-02-27 01:58:01 +0000

    new file:   01.py

---
+++ 01.py
@@ -0,0 +1,19 @@
+#program to demonstrate variable declration and basic operations
+a = 2 
+b = 6
+
+print("a =",a,",b =",b)
+print("Basic operations on a & b :\n")
+
+print("Addition of a and b is",a+b)
+print("Subtraction of a from b is",b-a)
+print("Multiplication of a and b is",a*b)
+print("Division of a by b is",a/b)
+print("Floor division of a by b is",a//b)
+print("Modulus of a by b is",a%b)
+print("Exponential of b with power a is",b**a)
+
+print("\nType of 'a' is ",type(a))
+a = 3.4
+print("a =",a)
+print("Type of 'a' now :",type(a))
