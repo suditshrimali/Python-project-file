4be6b615f5c48842bc3bb0f9ee59f08facd02a4c
Sudit Shrimali <suditshrimali@gmail.com>
2026-02-27 09:46:12 +0000

    new file:   2.py

---
+++ 2.py
@@ -0,0 +1,24 @@
+#program to use different types of operators
+a = 32
+b = 13
+print("a = ",a,"b = ",b)
+print("Arithmetic operators : \n")
+print("a + b =",a+b)
+print("a - b =",a-b)
+print("a * b =",a*b)
+print("a / b =",a/b)
+print("a ** b =",a**b)
+print("a // b =",a//b)
+print("a '%' b =",a%b)
+
+print("\nRelational operators : \n")
+print("Is a > b,",a>b)
+print("Is a < b,",a<b)
+print("Is a <= b,",a<=b)
+print("Is a >= b,",a>=b)
+print("Is a == b,",a==b)
+print("Is a != b,",a!=b)
+
+print("\nLogical operators : \n")
+print("Is (a>b) && (a<b),",(a>b)&(a<b))
+print("Is (a>b) || (a<b),",(a>b)|(a<b))
